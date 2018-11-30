00b305f7ca0bc82ac8aa621ebcc4173a92b88f31
Foxerlee <foxerlee1@gmail.com>
2018-11-30 14:52:19 +0800

    try predict but failed

--- Analyze.py
+++ Analyze.py
@@ -9,12 +9,16 @@ import pylab as pl
 # Z = [1, 2, 1, 1]
 
 
-data = pd.read_csv("train/01/PLC/plc.csv").values
-data = pd.read_csv("train/03/Sensor/{}.csv".format(1)).values
-print(len(data[:,0]))
+# data = pd.read_csv("train/02/PLC/plc.csv").values
+data = pd.read_csv("train/01/Sensor/{}.csv".format(46)).values
+# print(len(data[:,0]))
 # csv_nos = set(data[:,-1])
 # print(csv_nos)
-#
+spinde = data[:,0]
+spinde = np.fft.fft(spinde)
+index = [i for i in range(len(spinde))]
+plt.plot(index, spinde)
+plt.show()
 # a = {}
 # for d in data:
 #     if d[-1] in a:
@@ -42,37 +46,37 @@ print(len(data[:,0]))
 
 # plt.plot([i for i in range(len(spindle))], spindle)
 # plt.show()
-c_max = []
-c_min = []
-c_ave = []
-c_var = []
-c_std = []
-
-for i in range(2,3):
-    data = pd.read_csv("train/03/Sensor/{}.csv".format(i+1)).values
-    c_max.append(data[:,0].max())
-    c_min.append(data[:,0].min())
-    c_ave.append(data[:,0].mean())
-    c_var.append(data[:,0].var())
-    c_std.append(data[:,0].std())
-c_index = [i for i in range(len(c_max))]
-
-
-sampling_rate = 25600
-fft_size = len(data[:,0])
-xs = data[:,0][:fft_size]
-xf = np.fft.rfft(xs)/fft_size
-freqs = np.linspace(0, sampling_rate/2, fft_size/2+1)
-xfp = 20*np.log10(np.clip(np.abs(xf), 1e-20, 1e100))
-pl.figure(figsize=(8,4))
-pl.subplot(211)
-pl.plot([i for i in range(len(data[:,0]))][:fft_size], xs)
-pl.xlabel("time(s)")
-pl.subplot(212)
-pl.plot(freqs, xfp)
-pl.xlabel("frequency(Hz)")
-pl.subplots_adjust(hspace=0.4)
-pl.show()
+# c_max = []
+# c_min = []
+# c_ave = []
+# c_var = []
+# c_std = []
+#
+# for i in range(2,3):
+#     data = pd.read_csv("train/03/Sensor/{}.csv".format(i+1)).values
+#     c_max.append(data[:,0].max())
+#     c_min.append(data[:,0].min())
+#     c_ave.append(data[:,0].mean())
+#     c_var.append(data[:,0].var())
+#     c_std.append(data[:,0].std())
+# c_index = [i for i in range(len(c_max))]
+#
+#
+# sampling_rate = 25600
+# fft_size = len(data[:,0])
+# xs = data[:,0][:fft_size]
+# xf = np.fft.rfft(xs)/fft_size
+# freqs = np.linspace(0, sampling_rate/2, fft_size/2+1)
+# xfp = 20*np.log10(np.clip(np.abs(xf), 1e-20, 1e100))
+# pl.figure(figsize=(8,4))
+# pl.subplot(211)
+# pl.plot([i for i in range(len(data[:,0]))][:fft_size], xs)
+# pl.xlabel("time(s)")
+# pl.subplot(212)
+# pl.plot(freqs, xfp)
+# pl.xlabel("frequency(Hz)")
+# pl.subplots_adjust(hspace=0.4)
+# pl.show()
 # data = {"max": c_max, "min": c_min, "ave": c_ave, "var": c_var, "std": c_std, "index": c_index}
 # data = pd.DataFrame(data)
 # sns.pairplot(data, y_vars=["max", "min", "ave", "var", "std"], x_vars='index', size=12, aspect=1, kind='reg')
@@ -83,16 +87,16 @@ pl.show()
 # plt.scatter([i for i in range(48)], c_std, label='std')
 # plt.legend(loc='upper right')
 
-plt.plot([i for i in range(len(data[:,0]))], data[:,0])
-plt.show()
-
-# X = data[:,0]
-# Y = data[:,1]
-# Z = data[:,2]
+# plt.plot([i for i in range(len(data[:,0]))], data[:,0])
+# plt.show()
 #
-# plt.plot([i for i in range(len(X))], X)
-# # plt.plot([i for i in range(len(Y))], Y)
-# # plt.plot([i for i in range(len(Z))], Z)
-plt.show()
+# # X = data[:,0]
+# # Y = data[:,1]
+# # Z = data[:,2]
+# #
+# # plt.plot([i for i in range(len(X))], X)
+# # # plt.plot([i for i in range(len(Y))], Y)
+# # # plt.plot([i for i in range(len(Z))], Z)
+# plt.show()
 
 
--- train.py
+++ train.py
@@ -1,3 +1,22 @@
+# ---
+# jupyter:
+#   jupytext_format_version: '1.2'
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+#   language_info:
+#     codemirror_mode:
+#       name: ipython
+#       version: 3
+#     file_extension: .py
+#     mimetype: text/x-python
+#     name: python
+#     nbconvert_exporter: python
+#     pygments_lexer: ipython3
+#     version: 3.6.5
+# ---
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,7 +29,7 @@ from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Dense, Embedding
 from keras.layers import LSTM
-
+from sklearn.svm import SVR
 max_features = 6
 batch_size = 32
 
@@ -55,39 +74,50 @@ def features(id, length, dim, is_fft=False):
             sum += v * v
         denominator.append(sum / len(vs))
 
-    return np.vstack((kurtosis, skew, denominator)).T
+    return np.vstack((kurtosis, skew, variance, denominator)).T
 
 
 def main():
     data_x_1_t = features('01', 47, 0, False)
     data_x_1_f = features('01', 47, 0, True)
 
-    data = np.hstack((data_x_1_t, data_x_1_f))
-    data = data.reshape(data.shape[1], data.shape[0])
-    print(data.shape)
+    train_data = np.hstack((data_x_1_t, data_x_1_f))
+    # train_data = train_data.reshape(train_data.shape[1], train_data.shape[0])
+    print(train_data.shape)
 
-    label = [i for i in range(data.shape[1])]
+    train_label = [i for i in range(train_data.shape[0])]
 
+    data_x_2_t = features('02', 47, 0, False)
+    data_x_2_f = features('02', 47, 0, True)
+    test_data = np.hstack((data_x_2_t, data_x_2_f))
+    # test_data =
+    test_label = [i for i in range(test_data.shape[0])]
     print('Build model...')
-    model = Sequential()
-    model.add(Embedding(max_features, 128))
-    model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
-    model.add(Dense(1, activation='sigmoid'))
+    clf = SVR(gamma='scale', C=1.0, epsilon=0.2)
+    clf.fit(train_data, train_label)
+    pre = clf.predict(test_data)
 
-    # try using different optimizers and different optimizer configs
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
+    print(pre)
 
-    print('Train...')
-    model.fit(data, label,
-              batch_size=batch_size,
-              epochs=15,
-              validation_data=(data, label))
-    score, acc = model.evaluate(data, label,
-                                batch_size=batch_size)
-    print('Test score:', score)
-    print('Test accuracy:', acc)
+    # model = Sequential()
+    # model.add(Embedding(max_features, 128))
+    # model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+    # model.add(Dense(1, activation='sigmoid'))
+    #
+    # # try using different optimizers and different optimizer configs
+    # model.compile(loss='binary_crossentropy',
+    #               optimizer='adam',
+    #               metrics=['accuracy'])
+    #
+    # print('Train...')
+    # model.fit(data, label,
+    #           batch_size=batch_size,
+    #           epochs=15,
+    #           validation_data=(data, label))
+    # score, acc = model.evaluate(data, label,
+    #                             batch_size=batch_size)
+    # print('Test score:', score)
+    # print('Test accuracy:', acc)
     # data_x_2_t = features('02', 47, 0, False)
     # data_x_2_f = features('02', 47, )
 
